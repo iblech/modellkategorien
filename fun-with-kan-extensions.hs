4a75e820f8ecbd32bdc62bc681215368eb014117
Ingo Blechschmidt <iblech@web.de>
2015-04-19 23:00:42 +0200

    Koenden, Enden und Lan

--- fun-with-kan-extensions.hs
+++ fun-with-kan-extensions.hs
@@ -1,7 +1,10 @@
-{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE RankNTypes, ExistentialQuantification #-}
 module Main where
 
 newtype Ran k t a = MkRan { runRan :: forall r. (a -> k r) -> t r }
+-- folgt einfach aus der Ende-Formel für Ran und der Berechnung
+-- von Ende(S) als Menge der S-Keile mit Spitze 1.
+-- Siehe auch http://comonad.com/reader/2008/kan-extension-iii/.
 
 instance Functor (Ran k t) where
     fmap f phi = MkRan $ \u -> runRan phi (u . f)
@@ -9,4 +12,20 @@ instance Functor (Ran k t) where
 universal :: (Functor s) => (forall r. s (k r) -> t r) -> s a -> Ran k t a
 universal alpha v = MkRan $ \u -> alpha (fmap u v)
 
--- Weiter ausbauen!
+data Coend s = forall a. Coend (s a a)
+{-
+  Eigentlich sollte das lauten:
+
+      Coend (exists a. s a a / (lmap f x = rmap f x für alle f : a -> b, x : s a b))
+
+  Wegen Parametrizität respektieren aber Funktionen Coend f -> r (so wie
+  definiert) automatisch lmap/rmap.
+-}
+
+newtype End s = End { runEnd :: forall a. s a a }
+-- Hier ist es ähnlich. Die Werte vom Typ End s erfüllen automatisch die
+-- Keilbedingung.
+
+-- Ran k t c = End ((a,b) |-> (c -> k a) -> t b)
+
+-- Lan k t c = Coend ((a,b) |-> (k a -> c) x t b)
